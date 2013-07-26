5ed98c3173e60fd89813394f57741968144613e9
bartekfr <bartekfr@wp.pl>
2013-07-26 09:50:05 +0200

    remove commented line

--- js/tab.js
+++ js/tab.js
@@ -15,7 +15,6 @@
 			type: 'slide'
 		};
 		var settings = $.extend(true, {}, DEFAULT, options);
-		//var navItemSelector = settings.navItemSelector;
 		var navItems = $(settings.tabNavItemSelector, that);
 		var tabs = $(settings.tabContentItemSelector, that).addClass("tab-content-item");
 		var activeIndex = settings.activeIndex;
